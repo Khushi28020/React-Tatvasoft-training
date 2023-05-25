972164d63cc69cd12974db1842f221d0c71e4398
Khushi28020 <121652792+Khushi28020@users.noreply.github.com>
2023-05-25 15:46:10 +0530

    Add files via upload

---
+++ Day-1-2/src/components/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AboutUs = () => {
+  return (
+    <div>
+        <Navbar/>
+      <h1>About Us page</h1>
+    </div>
+  )
+}
+
+export default AboutUs
