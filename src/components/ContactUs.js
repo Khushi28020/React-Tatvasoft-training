d062670673a04cfef33af01ed45860ad8de49f87
Khushi28020 <121652792+Khushi28020@users.noreply.github.com>
2023-05-25 15:40:40 +0530

    Add files via upload

---
+++ src/components/ContactUs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ContactUs = () => {
+  return (
+    <div>
+        <Navbar/>
+      contact us
+    </div>
+  )
+}
+
+export default ContactUs
